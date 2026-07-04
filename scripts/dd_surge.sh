ea57e35c108660e7b210d0c999f07f19d18aa8cd
DDSynth-RL <noreply@github.com>
2026-07-04 19:00:23 +0800

    slightly change the sh script for surge training, but find surge xt might not be the most suitable synth for this task.

--- scripts/dd_surge.sh
+++ scripts/dd_surge.sh
@@ -14,7 +14,17 @@ env PYTHONPATH="$PROJECT_ROOT" "$PYTHON_BIN" -m src.train --config-name dd_train
   trainer.limit_val_batches=2 \
   model.validation.render_batches=2 \
   model.validation.nsynth_eval.batches=2 \
-  ckpt_path=outputs/dd_surge/2026-04-13_18-37-51/checkpoints/checkpoint_last.pt
+  ckpt_path=outputs/dd_surge/2026-04-15_17-17-38/checkpoints/checkpoint_last.pt
+
+# 2026-06-17: 在云南，发现huggingface连接出问题，暂时用镜像解决。
+export HF_ENDPOINT=https://hf-mirror.com
+export HF_HOME=~/hf_cache
+export HF_HUB_CACHE=~/hf_cache/hub
+python -m src.train --config-name dd_train_surge \
+  trainer.limit_val_batches=2 \
+  model.validation.render_batches=2 \
+  model.validation.nsynth_eval.batches=2 \
+  ckpt_path=outputs/dd_surge/2026-04-15_17-17-38/checkpoints/checkpoint_last.pt
 
 env PYTHONPATH="$PROJECT_ROOT" "$PYTHON_BIN" -m src.train --config-name dd_train_surge \
   trainer.limit_val_batches=2 \
